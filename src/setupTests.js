1b35feb6da0b39fe432427225aebb08c00f695ed
Michelle Panzironi <michelle.panzironi@gmail.com>
2022-12-09 18:31:39 -0500

    add setupTests

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,4 @@
-import "@testing-library/jest-dom/extend-expect";
-import fetchMock from "jest-fetch-mock";
+// import "@testing-library/jest-dom/extend-expect";
+// import fetchMock from "jest-fetch-mock";
 
-fetchMock.enableMocks();
+// fetchMock.enableMocks();
